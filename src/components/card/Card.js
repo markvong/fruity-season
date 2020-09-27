4bdb1a460d609d809255003a870b6e6c09b34935
Mark Vong <33412448+markvong@users.noreply.github.com>
2020-09-27 04:10:41 -0700

    Added padding to card description and increased default description length for testing.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -16,9 +16,13 @@ export const Card = (props) => {
             : "https://i.pinimg.com/originals/32/e2/41/32e2413585f1d2e0333c7dee3c4808bf.jpg"
         }
         alt="Img"
-        id="prod-img"
+        id="card-img"
       />
-      <p>{description ? description : "This is a default description."}</p>
+      <p id="card-description">
+        {description
+          ? description
+          : "This is a default description. More information to come for this particular card. Thank you for your patience."}
+      </p>
     </div>
   );
 };
