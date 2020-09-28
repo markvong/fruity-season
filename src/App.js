8b3f60029de7bced37041d846175f0a8d8bb63ad
Mark Vong <33412448+markvong@users.noreply.github.com>
2020-09-27 17:02:25 -0700

    Added footer with contact info

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import "./styles/App.scss";
 
 import { Container } from "./components/container/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTree, faTractor } from "@fortawesome/free-solid-svg-icons";
+import { faTree, faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        <FontAwesomeIcon className="icon" icon={faTree} size="xs" /> Seasonal
-        Crops
-      </h1>
+      <header id="header">
+        <h1>
+          <FontAwesomeIcon className="icon" icon={faTree} size="xs" /> Seasonal
+          Crops
+        </h1>
+      </header>
       <Container />
+      <footer id="footer">
+        <p>
+          <FontAwesomeIcon icon={faCopyright} id="footer-icon" size="md" />{" "}
+          Seasonal Crops by{" "}
+          <a href="https://github.com/themansbak" className="portfolio-link">
+            Alex Man
+          </a>{" "}
+          and{" "}
+          <a href="https://github.com/markvong" className="portfolio-link">
+            Mark Vong
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
